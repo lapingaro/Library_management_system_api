f188066c14f2355b991ad611fde7f952651518f1
lapingaro <chrisbehint@gmail.com>
2025-12-07 19:57:58 +0200

    GET Books from The database

--- server.js
+++ server.js
@@ -4,19 +4,42 @@ const Book = require ('./models/bookModels');
 const app = express ();
 const port = 3000;
 
-//Routes
+//Middle ware
 app.use (express.json ());
+
 app.get ('/', (req, res) => {
   res.send ('Hello From API');
 });
 
 //Book Routes //
-app.post ('/book', async (req, res) => {
+//POST To the Database
+app.post ('/books', async (req, res) => {
   try {
     const book = await Book.create (req.body);
     res.status (200).json (book);
   } catch (error) {
     console.log (error.message);
+    res.status (500).json ({message: error.message});
+  }
+});
+
+//GET Data From the Database
+app.get ('/books', async (req, res) => {
+  try {
+    const books = await Book.find ({});
+    res.status (200).json (books);
+  } catch (error) {
+    console.log (error.message);
+    res.status (500).json ({message: error.message});
+  }
+});
+//Get A single book from books
+app.get ('/books/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const book = await Book.findById (id);
+    res.status (200).json (book);
+  } catch (error) {
     res.status (500).json ({message: error.message});
   }
 });
