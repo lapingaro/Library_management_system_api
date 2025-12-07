0529c41279756b702aa276fcc21f7b1489d1cee3
lapingaro <chrisbehint@gmail.com>
2025-12-07 20:36:17 +0200

    Update a book info

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = 3000;
 
 //Middle ware
 app.use (express.json ());
+app.use (express.urlencoded ({extended: false}));
 
 app.get ('/', (req, res) => {
   res.send ('Hello From API');
@@ -44,6 +45,23 @@ app.get ('/books/:id', async (req, res) => {
   }
 });
 
+//UPDATE A Book
+app.put ('/books/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const book = await Book.findByIdAndUpdate (id, req.body);
+    //cannot find any product in the library database
+    if (!book) {
+      return res.status (404).json ({
+        message: `Cannot not find a book with that ID in the library ${id}`,
+      });
+    }
+    const updatedbook = await Book.findByIdAndUpdate (id, req.body);
+    res.status (200).json (updatedbook);
+  } catch (error) {
+    res.status (500).json ({message: error.message});
+  }
+});
 mongoose
   .connect (
     'mongodb+srv://root:Ramzi3650@devadictapi.fk56ax2.mongodb.net/library_management?appName=devadictapi'
